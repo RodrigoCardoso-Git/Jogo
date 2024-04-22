94916819f065b85cc8f4886f3bc90bfad6ac60f3
Rodrigo Cardoso <rodrigocar987@gmail.com>
2024-04-22 20:17:50 -0300

    Recomeçar o desenvolvimento do jogo

---
+++ dist/ProjetoJogo.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log('Começarei novamente do zero');
+console.log('Realizarei o projeto inicial no papel. Só depois irei fazer a implementação');
---
+++ typescript/ProjetoJogo.ts
@@ -0,0 +1,2 @@
+console.log('Começarei novamente do zero');
+console.log('Realizarei o projeto inicial no papel. Só depois irei fazer a implementação');
